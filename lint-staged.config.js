898a537fae9a2fc4807170a2483cd54113784181
Kotaro Sugawara <kotarella1110@gmail.com>
2018-07-11 15:19:49 +0900

    Added textlint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   linters: {
+    'docs/**': ['yarn lint:text --fix', 'git add'],
     '**/*.js': ['yarn lint:js --fix', 'git add'],
     '**/*.{css,html}': ['yarn lint:css --fix', 'git add'],
   },
