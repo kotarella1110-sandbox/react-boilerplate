ad3c1e84c4d4b7ca979562e269ad00f3ea39e619
Kotaro Sugawara <kotarella1110@gmail.com>
2018-07-12 10:16:06 +0900

    Fixed npm-scripts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   linters: {
     'docs/**': ['yarn lint:text --fix', 'git add'],
     '**/*.js': ['yarn lint:js --fix', 'git add'],
-    '**/*.{css,html}': ['yarn lint:css --fix', 'git add'],
+    '**/*.{css,scss,html}': ['yarn lint:css --fix', 'git add'],
   },
   ignore: ['/node_modules/**', '/build/**'],
 };
