11ff49912d7dd9c18e361a86cb7ce57c5e0b09ff
Kotaro Sugawara <kotarella1110@gmail.com>
2018-07-20 15:59:40 +0900

    Fixed setupTests.js Fixed "info addon and storyshot seem to be incompatible" https://github.com/storybooks/storybook/issues/1011

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,10 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+// https://github.com/storybooks/storybook/issues/1011
+jest.mock('@storybook/addon-info', () => ({
+  withInfo: () => storyFn => storyFn,
+  setDefaults: () => {},
+}));
+
 configure({ adapter: new Adapter() });
