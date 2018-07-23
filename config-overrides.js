5d1e0de14cc093e3bb4e8ec428abb63a55bf4045
Kotaro Sugawara <kotarella1110@gmail.com>
2018-07-23 20:10:12 +0900

    Added react-app-rewire-eslint react-scripts の eslint, eslint-plugin-import, eslint-plugin-jsx-a11y, eslint-plugin-react が重複し、 yarn start 以下のエラーが発生したため、 package.json に resolutions を指定してパッケージをオーバーライド https://stackoverflow.com/questions/40226639/how-do-i-override-nested-dependencies-with-yarn/41082766

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,10 @@
-const rewireStyledComponents = require('react-app-rewire-styled-components');
+const rewireEslint = require('react-app-rewire-eslint');
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const rewireStyledComponents = require('react-app-rewire-styled-components');
 
 module.exports = (config, env) => {
   let overrodeConfig = Object.assign({}, config);
+  overrodeConfig = rewireEslint(config, env);
   overrodeConfig = rewireReactHotLoader(overrodeConfig, env);
   overrodeConfig = rewireStyledComponents(config, env, {
     displayName: true,
