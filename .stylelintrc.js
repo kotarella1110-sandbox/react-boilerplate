ad7f666a97bd55bf4c6fd4635b1b50daa2b57f70
Kotaro Sugawara <kotarella1110@gmail.com>
2018-07-25 16:40:46 +0900

    Fixed .stylelintrc styled-components の .stylelintrc から stylelint-config-prettier を削除

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,4 @@
 module.exports = {
   processors: ['stylelint-processor-styled-components'],
-  extends: [
-    'stylelint-config-airbnb',
-    'stylelint-config-prettier',
-    'stylelint-config-styled-components',
-  ],
+  extends: ['stylelint-config-airbnb', 'stylelint-config-styled-components'],
 };
