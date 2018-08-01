fcc7d25658f844b6dc306c2da199e571b986b37e
Kotaro Sugawara <kotarella1110@gmail.com>
2018-08-01 17:36:19 +0900

    Added react-stubber and Fixed .storybook/config.js

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,21 @@
+import { setStubbingMode } from 'react-stubber';
+import { injectGlobal } from 'styled-components';
 import { configure, setAddon } from '@storybook/react';
 import '@storybook/addon-console';
 import { setOptions } from '@storybook/addon-options/preview';
 import JSXAddon from 'storybook-addon-jsx';
+
+setStubbingMode(true);
+
+injectGlobal([
+  `
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+`,
+]);
 
 setOptions({
   hierarchySeparator: /\//,
