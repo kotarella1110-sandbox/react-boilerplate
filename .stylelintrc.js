b3447195d35a817efdfc06dc8f93d22bf70951d9
Kotaro Sugawara <kotarella1110@gmail.com>
2018-08-03 09:35:03 +0900

    Fixed .stylelintrc.js for styled-components

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   processors: ['stylelint-processor-styled-components'],
   extends: ['stylelint-config-airbnb', 'stylelint-config-styled-components'],
+  rules: {
+    'number-leading-zero': 'always',
+    'rule-empty-line-before': null,
+    'string-quotes': 'single',
+    'order/order': [
+      { type: 'at-rule' },
+      { type: 'at-rule', hasBlock: true },
+      'rules',
+    ],
+  },
 };
