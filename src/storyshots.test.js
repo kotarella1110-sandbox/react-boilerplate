54bb1dd267702d741833300d39146da6b68cf84f
Kotaro Sugawara <kotarella1110@gmail.com>
2018-08-29 11:33:50 +0900

    build(storyshots): enabled stubber in storyshots config

--- src/storyshots.test.js
+++ src/storyshots.test.js
@@ -1,8 +1,11 @@
 import initStoryshots, {
   multiSnapshotWithOptions,
 } from '@storybook/addon-storyshots';
+import { setStubbingMode } from 'react-stubber';
 import styleSheetSerializer from 'jest-styled-components/src/styleSheetSerializer';
 import { addSerializer } from 'jest-snapshot';
+
+setStubbingMode(true);
 
 addSerializer(styleSheetSerializer);
 
